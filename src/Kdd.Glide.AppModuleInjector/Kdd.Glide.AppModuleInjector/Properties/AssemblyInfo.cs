779d5fb4e2cad8bd639e554385670b6315366d75
Taras Shevchuk <t.shevchuk@kdd-digital.de>
2020-10-28 14:08:55 +0200

    Migrated native project to AndroidX Updated Gradle to version 4.11.0 Code cleanup

--- src/Kdd.Glide.AppModuleInjector/Kdd.Glide.AppModuleInjector/Properties/AssemblyInfo.cs
+++ src/Kdd.Glide.AppModuleInjector/Kdd.Glide.AppModuleInjector/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KDD Digital Healthcare GmbH")]
 [assembly: AssemblyProduct("Kdd.Glide.AppModuleInjector")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
